ac0fcd6b701caa9f5c5beb03021744544e23420a
Reyna DeLoge <reyna.deloge@gmail.com>
2016-10-15 19:19:52 -0600

    ammend last commit with application.rb change

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module ReactRailsBase
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Gives access to state helpers through React.addons.update
     config.react.addons = true
   end
 end
