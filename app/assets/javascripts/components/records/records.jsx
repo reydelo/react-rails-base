73720bd4a2523bca6b42d19a00a2e830fefefb4d
Reyna DeLoge <reyna.deloge@gmail.com>
2016-10-15 18:52:43 -0600

    create reusable components for amount indicators

--- app/assets/javascripts/components/records/records.jsx
+++ app/assets/javascripts/components/records/records.jsx
@@ -17,11 +17,41 @@ let Records = React.createClass({
     this.setState({records: records});
   },
 
+  credits: function() {
+    let credits = this.state.records.filter(function(val) {
+      return val.amount >= 0;
+    });
+    return credits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount);
+    }, 0);
+  },
+
+  debits: function() {
+    let debits = this.state.records.filter(function(val) {
+      return val.amount < 0
+    });
+    return debits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount)
+    }, 0);
+  },
+
+  balance: function() {
+    return this.debits() + this.credits();
+  },
+
   render: function() {
     return(
       <div className='records'>
         <h2 className='title'>Records</h2>
+
+        <div className='row'>
+          <AmountBox type='success' amount={this.credits()} text='Credit' />
+          <AmountBox type='danger' amount={this.debits()} text='Debit' />
+          <AmountBox type='info' amount={this.balance()} text='Balance' />
+        </div>
+
         <RecordForm handleNewRecord={this.addRecord} />
+
         <table className='table table-bordered'>
           <thead>
             <tr>
@@ -36,6 +66,7 @@ let Records = React.createClass({
             })}
           </tbody>
         </table>
+
       </div>
     )
   }
